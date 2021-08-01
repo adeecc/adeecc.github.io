8607bf3fd231819e90acafa7fbf14d677f226cac
Aditya Chopra <adeecc11@gmail.com>
2021-08-02 01:57:19 +0530

    Temporary Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,76 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import Link from 'next/link';
+import { Disclosure, Transition } from '@headlessui/react';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+interface NavItem {
+  name: string;
+  href: string;
+  current: boolean;
+}
+
+const navigation: NavItem[] = [
+  { name: 'About', href: '#', current: true },
+  { name: 'Experience', href: '#', current: false },
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Contact', href: '#', current: false },
+];
 
 interface Props {}
 
 const NavBar = (props: Props) => {
   return (
-    <>
-      <h1>Hello There Hoes!</h1>
-    </>
+    <Disclosure as='nav' className=''>
+      {({ open }) => (
+        <>
+          <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
+            <div className='relative flex items-center justify-between h-16'>
+              <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
+                {/* Mobile Menu Button */}
+                <Disclosure.Button className='inline-flex items-center justify-center p-2 rounded-md text-primary-800 hover:text-primary-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+                  {open ? (
+                    <XIcon className='block h-6 w-6' aria-hidden='true' />
+                  ) : (
+                    <MenuIcon className='block h-6 w-6' aria-hidden='true' />
+                  )}
+                </Disclosure.Button>
+              </div>
+
+              <div className='flex-1 flex items-center justify-center sm:items-stretch:sm:justify-start'>
+                <div className='flex-shrink-0 flex items-center'>
+                  <img
+                    className='block lg:hidden h-8 w-auto'
+                    src='https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg'
+                    alt='Workflow'
+                  />
+                  <img
+                    className='hidden lg:block h-8 w-auto'
+                    src='https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg'
+                    alt='Workflow'
+                  />
+                </div>
+                <div className='hidden sm:block sm:ml-6'>
+                  <div className='flex space-x-4'>
+                    {navigation.map((item) => (
+                      <Link key={item.name} href={item.href}>
+                        <a className='text-primary-900'>{item.name}</a>
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className='sm:hidden'>
+            {navigation.map((item) => (
+              <Link key={item.name} href={item.href}>
+                <a className='text-primary-900'>{item.name}</a>
+              </Link>
+            ))}
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 };
 
