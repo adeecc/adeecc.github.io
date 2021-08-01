0be87b776aaf84595f5ae288288559ed8637a7d8
Aditya Chopra <adeecc11@gmail.com>
2021-08-02 02:46:30 +0530

    Finish NavBar Design for Desktop

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,7 @@ import React, { Fragment, useState } from 'react';
 import Link from 'next/link';
 import { Disclosure, Transition } from '@headlessui/react';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import Button from './Button';
 interface NavItem {
   name: string;
   href: string;
@@ -19,54 +20,75 @@ interface Props {}
 
 const NavBar = (props: Props) => {
   return (
-    <Disclosure as='nav' className=''>
+    <Disclosure as='nav'>
       {({ open }) => (
         <>
-          <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
-            <div className='relative flex items-center justify-between h-16'>
-              <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
-                {/* Mobile Menu Button */}
-                <Disclosure.Button className='inline-flex items-center justify-center p-2 rounded-md text-primary-800 hover:text-primary-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
-                  {open ? (
-                    <XIcon className='block h-6 w-6' aria-hidden='true' />
-                  ) : (
-                    <MenuIcon className='block h-6 w-6' aria-hidden='true' />
-                  )}
-                </Disclosure.Button>
-              </div>
-
-              <div className='flex-1 flex items-center justify-center sm:items-stretch:sm:justify-start'>
+          <div className='fixed w-screen px-6 lg:px-8'>
+            <div className='relative flex items-center justify-between h-20'>
+              {/* Logo */}
+              <div className='flex-1 h-full flex items-center justify-start sm:items-stretch sm:justify-start'>
                 <div className='flex-shrink-0 flex items-center'>
                   <img
-                    className='block lg:hidden h-8 w-auto'
+                    className='h-8 w-auto'
                     src='https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg'
                     alt='Workflow'
                   />
-                  <img
-                    className='hidden lg:block h-8 w-auto'
-                    src='https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg'
-                    alt='Workflow'
-                  />
-                </div>
-                <div className='hidden sm:block sm:ml-6'>
-                  <div className='flex space-x-4'>
-                    {navigation.map((item) => (
-                      <Link key={item.name} href={item.href}>
-                        <a className='text-primary-900'>{item.name}</a>
-                      </Link>
-                    ))}
-                  </div>
                 </div>
               </div>
+
+              {/* Menu for big screens */}
+              <div className='hidden sm:block sm:h-full'>
+                {/** inline block instead? */}
+                <div className='flex h-full space-x-4 items-center'>
+                  {navigation.map((item, index) => (
+                    <Link key={item.name} href={item.href}>
+                      <a>
+                        <div className='font-mono text-accent-800 inline'>
+                          {String(index).padStart(2, '0') + '. '}
+                        </div>
+                        <div className='text-primary-100 hover:text-accent-800 inline'>
+                          {item.name}
+                        </div>
+                      </a>
+                    </Link>
+                  ))}
+                  <Button href='#'>Resume</Button>
+                </div>
+              </div>
+
+              {/* Button for Mobile */}
+              <Disclosure.Button className='flex items-center sm:hidden'>
+                {open ? (
+                  <XIcon
+                    className='text-primary-100 block h-6 w-6'
+                    aria-hidden='true'
+                  />
+                ) : (
+                  <MenuIcon
+                    className='text-primary-100 block h-6 w-6'
+                    aria-hidden='true'
+                  />
+                )}
+              </Disclosure.Button>
             </div>
           </div>
 
           <Disclosure.Panel className='sm:hidden'>
-            {navigation.map((item) => (
-              <Link key={item.name} href={item.href}>
-                <a className='text-primary-900'>{item.name}</a>
-              </Link>
-            ))}
+            <div className='flex flex-col px-2 pt-2 pb-3 space-y-4 items-center'>
+              {navigation.map((item, index) => (
+                <Link key={item.name} href={item.href}>
+                  <a>
+                    <div className='font-mono text-accent-800 inline'>
+                      {String(index).padStart(2, '0') + '. '}
+                    </div>
+                    <div className='text-primary-100 hover:text-accent-800 inline'>
+                      {item.name}
+                    </div>
+                  </a>
+                </Link>
+              ))}
+              <Button href='#'>Resume</Button>
+            </div>
           </Disclosure.Panel>
         </>
       )}
