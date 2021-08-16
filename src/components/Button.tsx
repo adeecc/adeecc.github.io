8887c437ce6651c137cb23f7c02bab3dc13ae75e
Aditya Chopra <adeecc11@gmail.com>
2021-08-16 16:15:03 +0530

    Add transitions

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ interface Props {
 const Button: React.FC<Props> = ({ href, children }) => {
   return (
     <Link href={href}>
-      <a className='py-4 px-6 border rounded-sm border-accent-900 text-accent-900'>
+      <a className='py-4 px-6 border rounded-sm border-accent-900 text-accent-900 transition transition-duration-150 ease-in hover:border-accent-100 hover:text-accent-100'>
         {children}
       </a>
     </Link>
